eb2414c36b874e201e201db7e782af5ca6817b1a
arianna.zafarana <arianna.zafarana@accenture.com>
2022-09-30 13:19:23 +0200

    adds delivery push internal basepath

--- src/main/java/it/pagopa/pn/radd/exception/PnInvalidInputException.java
+++ src/main/java/it/pagopa/pn/radd/exception/PnInvalidInputException.java
@@ -1,9 +1,5 @@
 package it.pagopa.pn.radd.exception;
 
-import it.pagopa.pn.commons.exceptions.PnHttpResponseException;
-import org.springframework.http.HttpStatus;
-
-
 public class PnInvalidInputException extends PnException {
 
     public PnInvalidInputException() {

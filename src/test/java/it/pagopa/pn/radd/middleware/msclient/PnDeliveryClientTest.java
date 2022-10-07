db8573e42906672c6f22c1a56adac5d8f749ef41
pasqualino.cristaudo <>
2022-10-07 14:08:26 +0200

    Uploaded and completed client' test classes and webhook.json

--- src/test/java/it/pagopa/pn/radd/middleware/msclient/PnDeliveryClientTest.java
+++ src/test/java/it/pagopa/pn/radd/middleware/msclient/PnDeliveryClientTest.java
@@ -22,15 +22,18 @@ class PnDeliveryClientTest extends BaseTest {
     @Test
     void testGetCheckAar() {
         String recipientType = "PF", recipientInternalId = "PF-4fc75df3-0913-407e-bdaa-e50329708b7d", qrCode = "UFVNUS1ETVdHLUhSTFAtMjAyMjA5LVEtMV9GUk1UVFI3Nk0wNkI3MTVFXzVhZGIxMGE2LTM1MDEtNDcyYS04ZTkyLTU3ZGUyYzgxNTZhYw";
-        Mono<ResponseCheckAarDtoDto> response = pnDeliveryClient.getCheckAar(recipientType, recipientInternalId, qrCode);
-        assertEquals("LJLH-GNTJ-DVXR-202209-J-1", response.block().getIun());
+        Mono<ResponseCheckAarDtoDto> monoResponse = pnDeliveryClient.getCheckAar(recipientType, recipientInternalId, qrCode);
+        monoResponse.map(response -> {
+            assertEquals("LJLH-GNTJ-DVXR-202209-J-1", response.getIun());
+            return Mono.empty();
+        }).block();
     }
 
     @Test
     void testGetCheckAarCode400() {
         String recipientType = "PF", recipientInternalId = "PG-4fc75df3-0913-407e-bdaa-e50329708b7d", qrCode = "UFVNUS1ETVdHLUhSTFAtMjAyMjA5LVEtMV9GUk1UVFI3Nk0wNkI3MTVFXzVhZGIxMGE2LTM1MDEtNDcyYS04ZTkyLTU3ZGUyYzgxNTZhYw";
-        Mono<ResponseCheckAarDtoDto> response = pnDeliveryClient.getCheckAar(recipientType, recipientInternalId, qrCode);
-        response.onErrorResume(RaddGenericException.class, exception -> {
+        Mono<ResponseCheckAarDtoDto> monoResponse = pnDeliveryClient.getCheckAar(recipientType, recipientInternalId, qrCode);
+        monoResponse.onErrorResume(RaddGenericException.class, exception -> {
             assertEquals(CF_OR_QRCODE_NOT_VALID, exception.getExceptionType());
             return Mono.empty();
         }).block();
@@ -40,10 +43,28 @@ class PnDeliveryClientTest extends BaseTest {
     void testGetNotifications() {
         String iun = "LJLH-GNTJ-DVXR-202209-J-1";
         Mono<SentNotificationDto> monoResponse = pnDeliveryClient.getNotifications(iun);
-        monoResponse.doOnNext(response -> {
+        monoResponse.map(response -> {
+            assertNotEquals(0, response.getRecipients().size());
             assertEquals(iun, response.getIun());
             assertEquals("CFComuneMilano", response.getSenderTaxId());
             assertEquals("comune di milano", response.getSenderDenomination());
+
+            response.getRecipients().forEach(element -> {
+                assertEquals("FRMTTR76M06B715E", element.getTaxId());
+                assertEquals("Mario Cucumber denomination", element.getDenomination());
+                assertNotNull(element.getPayment());
+                assertNotNull(element.getDigitalDomicile());
+                assertNotNull(element.getPhysicalAddress());
+                assertNotNull(element.getRecipientType());
+            });
+
+            response.getDocuments().forEach(element -> {
+                assertEquals("0", element.getDocIdx());
+                assertNotNull(element.getDigests());
+                assertNotNull(element.getRef());
+            });
+
+            return Mono.empty();
         }).block();
     }
 
@@ -61,18 +82,20 @@ class PnDeliveryClientTest extends BaseTest {
     void testGetPresignedUrlDocument() {
         String iun = "LJLH-GNTJ-DVXR-202209-J-1", docXid = "12984594", recipientTaxId = "65df2qm7y";
         Mono<NotificationAttachmentDownloadMetadataResponseDto> monoResponse = pnDeliveryClient.getPresignedUrlDocument(iun, docXid, recipientTaxId);
-        monoResponse.doOnNext(response -> {
+        monoResponse.map(response -> {
             assertEquals("D73JG1340FJ3GBFI04NT0B73JV9W7331V", response.getSha256());
             assertEquals("application/pdf", response.getContentType());
             assertEquals(54092, response.getContentLength());
+            assertEquals("http://downdocuments", response.getUrl());
+            assertEquals(8035, response.getRetryAfter());
+            return Mono.empty();
         }).block();
     }
 
     @Test
     void testGetPresignedUrlDocumentCode400() {
-        String iun = "LJLH-GNTJ-DVXR-202209-J-1", docXid = "12984594", recipientTaxId = "65df2qm7y";
+        String iun = "LJLH-GNTJ-DVXR-202209-J-1", docXid = "12984594", recipientTaxId = "";
         Mono<NotificationAttachmentDownloadMetadataResponseDto> response = pnDeliveryClient.getPresignedUrlDocument(iun, docXid, recipientTaxId);
-
         response.onErrorResume(exception -> {
             if (exception instanceof PnRaddException){
                 assertEquals(HttpStatus.valueOf(400), ((PnRaddException) exception).getWebClientEx().getStatusCode());
@@ -87,16 +110,17 @@ class PnDeliveryClientTest extends BaseTest {
     void testGetPresignedUrlPaymentDocument() {
         String iun = "LJLH-GNTJ-DVXR-202209-J-1", attachmentName = "paymentDoc", recipientTaxId = "12df2qm7y";
         Mono<NotificationAttachmentDownloadMetadataResponseDto> monoResponse = pnDeliveryClient.getPresignedUrlPaymentDocument(iun, attachmentName, recipientTaxId);
-        monoResponse.doOnNext(response -> {
+        monoResponse.map(response -> {
             assertEquals("D73JG1340FJ3GBFI04NT0B73JV9W7331V", response.getSha256());
             assertEquals("application/pdf", response.getContentType());
             assertEquals(54092, response.getContentLength());
+            return Mono.empty();
         }).block();
     }
 
     @Test
     void testGetPresignedUrlPaymentDocumentCode400() {
-        String iun = "LJLH-GNTJ-DVXR-202209-J-1", attachmentName = "paymentDoc", recipientTaxId = "12df2qm7y";
+        String iun = "LJLH-GNTJ-DVXR-202209-J-1", attachmentName = "paymentDoc", recipientTaxId = "";
         Mono<NotificationAttachmentDownloadMetadataResponseDto> response = pnDeliveryClient.getPresignedUrlPaymentDocument(iun, attachmentName, recipientTaxId);
         response.onErrorResume(exception -> {
             if (exception instanceof PnRaddException){
--- src/test/java/it/pagopa/pn/radd/middleware/msclient/PnDeliveryPushClientTest.java
+++ src/test/java/it/pagopa/pn/radd/middleware/msclient/PnDeliveryPushClientTest.java
@@ -1,23 +1,21 @@
 package it.pagopa.pn.radd.middleware.msclient;
 
 import it.pagopa.pn.radd.config.BaseTest;
-import it.pagopa.pn.radd.exception.ExceptionCodeEnum;
-import it.pagopa.pn.radd.exception.RaddGenericException;
+import it.pagopa.pn.radd.exception.PnRaddException;
 import it.pagopa.pn.radd.microservice.msclient.generated.pndatavault.v1.dto.RecipientTypeDto;
 import it.pagopa.pn.radd.microservice.msclient.generated.pndeliverypush.internal.v1.dto.ResponseNotificationViewedDtoDto;
+import it.pagopa.pn.radd.microservice.msclient.generated.pndeliverypush.internal.v1.dto.ResponsePaperNotificationFailedDtoDto;
 import it.pagopa.pn.radd.middleware.db.entities.RaddTransactionEntity;
-import it.pagopa.pn.radd.utils.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Date;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PnDeliveryPushClientTest extends BaseTest {
-
     @Autowired
     PnDeliveryPushClient pnDeliveryPushClient;
 
@@ -29,28 +27,53 @@ class PnDeliveryPushClientTest extends BaseTest {
         entity.setOperationStartDate("2022-09-30T13:57:00.000");
         entity.setRecipientId("1924814");
         Mono<ResponseNotificationViewedDtoDto> monoResponse = pnDeliveryPushClient.notifyNotificationViewed(entity);
-        monoResponse.doOnNext(response -> {
-            assertEquals(entity.getIun(), response.getIun());
-        }).block();
+        monoResponse.doOnNext(response -> assertEquals(entity.getIun(), response.getIun())).block();
     }
 
-    //TODO webohook return 404, the request not found
-    //@Test
+    @Test
     void testNotifyNotificationViewedCode400() {
         RaddTransactionEntity entity = new RaddTransactionEntity();
         entity.setIun("LJLH-GNTJ-DVXR-202209-J-1");
         entity.setRecipientType(RecipientTypeDto.PF.getValue());
-        entity.setOperationStartDate(DateUtils.formatDate(new Date()));
-        entity.setRecipientId("1924814");
+        entity.setOperationStartDate("2022-09-30T13:57:00.000");
+        entity.setRecipientId("");
         Mono<ResponseNotificationViewedDtoDto> response = pnDeliveryPushClient.notifyNotificationViewed(entity);
 
         response.onErrorResume(exception -> {
-            if (exception instanceof RaddGenericException){
-                assertEquals(ExceptionCodeEnum.KO, ((RaddGenericException) exception).getCode());
+            if (exception instanceof PnRaddException){
+                assertEquals(400, ((PnRaddException) exception).getWebClientEx().getStatusCode().value());
                 return Mono.empty();
             }
             fail("Badly type exception");
             return null;
         }).block();
+    }
+
+    @Test
+    void testPaperNotificationFailed() {
+        String recipientInternalId  = "854Bgs31a";
+        Mono<List<ResponsePaperNotificationFailedDtoDto>> monoResponse = pnDeliveryPushClient.getPaperNotificationFailed(recipientInternalId).collectList();
+        monoResponse.map(response -> {
+            assertNotEquals(0, response.size());
+            response.forEach(element -> {
+                assertEquals("LJLH-GNTJ-DVXR-202209-J-1", element.getIun());
+                assertEquals("854Bgs31a", element.getRecipientInternalId());
+                assertEquals("http://aarUrl", element.getAarUrl());
+            });
+            return Mono.empty();
+        }).block();
+    }
+    @Test
+    void testPaperNotificationFailedCode400() {
+        String recipientInternalId  = "854";
+        Flux<ResponsePaperNotificationFailedDtoDto> response = pnDeliveryPushClient.getPaperNotificationFailed(recipientInternalId);
+        response.onErrorResume(exception -> {
+            if (exception instanceof PnRaddException){
+                assertEquals(400, ((PnRaddException) exception).getWebClientEx().getStatusCode().value());
+                return Mono.empty();
+            }
+            fail("Badly type exception");
+            return Mono.empty();
+        }).blockFirst();
     }
 }

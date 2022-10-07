6fc59da811c4da60349f6bbffaa48dd55204a34c
cristopher.moccia <cristopher.moccia@accenture.com>
2022-10-07 10:34:54 +0200

    added retry after response

--- src/main/java/it/pagopa/pn/radd/mapper/StartTransactionResponseMapper.java
+++ src/main/java/it/pagopa/pn/radd/mapper/StartTransactionResponseMapper.java
@@ -1,10 +1,12 @@
 package it.pagopa.pn.radd.mapper;
 
+import it.pagopa.pn.radd.exception.ExceptionTypeEnum;
 import it.pagopa.pn.radd.exception.RaddGenericException;
 import it.pagopa.pn.radd.rest.radd.v1.dto.StartTransactionResponse;
 import it.pagopa.pn.radd.rest.radd.v1.dto.StartTransactionResponseStatus;
 import it.pagopa.pn.radd.utils.Const;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class StartTransactionResponseMapper {
@@ -28,6 +30,10 @@ public class StartTransactionResponseMapper {
         StartTransactionResponseStatus status = new StartTransactionResponseStatus();
         status.setMessage(ex.getExceptionType().getMessage());
         status.setCode(StartTransactionResponseStatus.CodeEnum.NUMBER_99);
+        if (ex.getExceptionType() == ExceptionTypeEnum.RETRY_AFTER){
+            status.setCode(StartTransactionResponseStatus.CodeEnum.NUMBER_2);
+            status.setRetryAfter((BigDecimal) ex.getExtra());
+        }
         response.setStatus(status);
         return response;
     }

2450c69fff531fc8be9a11d0b49560531746b78b
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2024-01-31 12:20:29 +0100

    updated attributes, indexes and added localstack queues

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -1,73 +1,45 @@
-echo "### CREATE RADD TRANSACTION TABLE ###"
+echo "### CREATE RADD ALT TRANSACTION TABLE ###"
 
 aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
     dynamodb create-table \
-    --table-name RaddTransactionDynamoTable \
+    --table-name pn-radd-transaction-alt \
     --attribute-definitions \
-        AttributeName=operationId,AttributeType=S \
+        AttributeName=transactionId,AttributeType=S \
         AttributeName=operationType,AttributeType=S \
+        AttributeName=iun,AttributeType=S \
         AttributeName=recipientId,AttributeType=S \
         AttributeName=delegateId,AttributeType=S \
-        AttributeName=qrCode,AttributeType=S \
-        AttributeName=iun,AttributeType=S \
     --key-schema \
-        AttributeName=operationId,KeyType=HASH \
+        AttributeName=transactionId,KeyType=HASH \
         AttributeName=operationType,KeyType=RANGE \
-    --provisioned-throughput \
-        ReadCapacityUnits=10,WriteCapacityUnits=5 \
+    --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5 \
     --global-secondary-indexes \
-    "[
-        {
-            \"IndexName\": \"iun-transaction-index\",
-            \"KeySchema\": [{\"AttributeName\":\"iun\",\"KeyType\":\"HASH\"}],
-            \"Projection\":{
-                \"ProjectionType\":\"ALL\"
+        '[
+            {
+                "IndexName": "iun-transaction-index",
+                "KeySchema": [{"AttributeName":"iun","KeyType":"HASH"}],
+                "Projection": {"ProjectionType":"ALL"},
+                "ProvisionedThroughput": {"ReadCapacityUnits": 10, "WriteCapacityUnits": 5}
             },
-            \"ProvisionedThroughput\": {
-                \"ReadCapacityUnits\": 10,
-                \"WriteCapacityUnits\": 5
-            }
-        },
-        {
-            \"IndexName\": \"recipient-transaction-index\",
-            \"KeySchema\": [{\"AttributeName\":\"recipientId\",\"KeyType\":\"HASH\"}],
-            \"Projection\":{
-                \"ProjectionType\":\"ALL\"
+            {
+                "IndexName": "recipient-transaction-index",
+                "KeySchema": [{"AttributeName":"recipientId","KeyType":"HASH"}],
+                "Projection": {"ProjectionType":"ALL"},
+                "ProvisionedThroughput": {"ReadCapacityUnits": 10, "WriteCapacityUnits": 5}
             },
-            \"ProvisionedThroughput\": {
-                \"ReadCapacityUnits\": 10,
-                \"WriteCapacityUnits\": 5
-            }
-        },
-        {
-            \"IndexName\": \"delegate-transaction-index\",
-            \"KeySchema\": [{\"AttributeName\":\"delegateId\",\"KeyType\":\"HASH\"}],
-            \"Projection\":{
-                \"ProjectionType\":\"ALL\"
+            {
+                "IndexName": "delegate-transaction-index",
+                "KeySchema": [{"AttributeName":"delegateId","KeyType":"HASH"}],
+                "Projection": {"ProjectionType":"ALL"},
+                "ProvisionedThroughput": {"ReadCapacityUnits": 10, "WriteCapacityUnits": 5}
             },
-            \"ProvisionedThroughput\": {
-                \"ReadCapacityUnits\": 10,
-                \"WriteCapacityUnits\": 5
-            }
-        },
-        {
-            \"IndexName\": \"qrcode-transaction-index\",
-            \"KeySchema\": [{\"AttributeName\":\"qrCode\",\"KeyType\":\"HASH\"}],
-            \"Projection\":{
-                \"ProjectionType\":\"ALL\"
-            },
-            \"ProvisionedThroughput\": {
-                \"ReadCapacityUnits\": 10,
-                \"WriteCapacityUnits\": 5
-            }
-        }
-    ]"
+        ]'
 
 echo "### CREATE OPERATIONS AND IUNS TABLE ###"
 
 aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
     dynamodb create-table \
-    --table-name OperationsAndIunsDynamoTable \
+    --table-name pn-operations-iuns-alt \
     --attribute-definitions \
         AttributeName=id,AttributeType=S \
         AttributeName=operationId,AttributeType=S \
@@ -80,7 +52,10 @@ aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
     "[
         {
             \"IndexName\": \"iun-and-operation-index\",
-            \"KeySchema\": [{\"AttributeName\":\"iun\",\"KeyType\":\"HASH\"}, {\"AttributeName\":\"operationId\",\"KeyType\":\"RANGE\"}],
+            \"KeySchema\": [
+                {\"AttributeName\":\"iun\",\"KeyType\":\"HASH\"},
+                {\"AttributeName\":\"operationId\",\"KeyType\":\"RANGE\"}
+            ],
             \"Projection\":{
                 \"ProjectionType\":\"ALL\"
             },
@@ -91,4 +66,13 @@ aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
         }
     ]"
 
+    echo "### CREATE QUEUES FOR RADD-ALT ###"
+    queues="pn-safestore_to_radd_alt"
+    for qn in $(echo $queues | tr " " "\n"); do
+      echo creating queue $qn ...
+      aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+        sqs create-queue \
+        --attributes '{"DelaySeconds":"2"}' \
+        --queue-name $qn
+    done
 echo "Initialization terminated"

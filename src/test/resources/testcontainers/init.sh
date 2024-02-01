b47f427311373386c1aa053aa5bbb25fbb23254d
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2024-02-01 14:53:36 +0100

    deleted queue from script

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -65,14 +65,4 @@ aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
             }
         }
     ]"
-
-    echo "### CREATE QUEUES FOR RADD-ALT ###"
-    queues="pn-safestore_to_radd_alt"
-    for qn in $(echo $queues | tr " " "\n"); do
-      echo creating queue $qn ...
-      aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
-        sqs create-queue \
-        --attributes '{"DelaySeconds":"2"}' \
-        --queue-name $qn
-    done
 echo "Initialization terminated"

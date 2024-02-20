6dea5ba70868c1a21ed027faa0f1e1194fa1ed1d
marcello.satta <marcello.satta@nttdata.com>
2024-02-20 17:49:52 +0100

    small fixes: junit tests

--- src/test/java/it/pagopa/pn/radd/mapper/ActInquiryResponseMapperTest.java
+++ src/test/java/it/pagopa/pn/radd/mapper/ActInquiryResponseMapperTest.java
@@ -2,11 +2,15 @@ package it.pagopa.pn.radd.mapper;
 
 import it.pagopa.pn.radd.alt.generated.openapi.server.v1.dto.ActInquiryResponse;
 import it.pagopa.pn.radd.alt.generated.openapi.server.v1.dto.ActInquiryResponseStatus;
+import it.pagopa.pn.radd.exception.ExceptionTypeEnum;
+import it.pagopa.pn.radd.exception.IunAlreadyExistsException;
 import it.pagopa.pn.radd.exception.RaddGenericException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static it.pagopa.pn.radd.exception.ExceptionTypeEnum.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ActInquiryResponseMapperTest {
@@ -53,5 +57,84 @@ class ActInquiryResponseMapperTest {
         response = ActInquiryResponseMapper.fromException(ex);
         status.setCode(ActInquiryResponseStatus.CodeEnum.NUMBER_99);
         assertEquals("Error", response.getStatus().getMessage());
+    }
+
+    /**
+     * Method under test: {@link ActInquiryResponseMapper#fromException(RaddGenericException)}
+     */
+    @Test
+    void testFromException() {
+        ActInquiryResponse actualFromExceptionResult = ActInquiryResponseMapper
+                .fromException(new RaddGenericException(ExceptionTypeEnum.IUN_NOT_FOUND));
+        assertFalse(actualFromExceptionResult.getResult());
+        ActInquiryResponseStatus status = actualFromExceptionResult.getStatus();
+        assertEquals(ActInquiryResponseStatus.CodeEnum.NUMBER_99, status.getCode());
+        assertEquals("Iun not found with params", status.getMessage());
+    }
+
+    /**
+     * Method under test: {@link ActInquiryResponseMapper#fromException(RaddGenericException)}
+     */
+    @Test
+    void testFromException2() {
+        ActInquiryResponse actualFromExceptionResult = ActInquiryResponseMapper
+                .fromException(new IunAlreadyExistsException());
+        assertFalse(actualFromExceptionResult.getResult());
+        ActInquiryResponseStatus status = actualFromExceptionResult.getStatus();
+        assertEquals(ActInquiryResponseStatus.CodeEnum.NUMBER_3, status.getCode());
+        assertEquals("Stampa già eseguita", status.getMessage());
+    }
+
+
+    /**
+     * Method under test: {@link ActInquiryResponseMapper#fromException(RaddGenericException)}
+     */
+    @Test
+    void testFromException4() {
+        ActInquiryResponse actualFromExceptionResult = ActInquiryResponseMapper
+                .fromException(new RaddGenericException("An error occurred"));
+        assertFalse(actualFromExceptionResult.getResult());
+        ActInquiryResponseStatus status = actualFromExceptionResult.getStatus();
+        assertEquals(ActInquiryResponseStatus.CodeEnum.NUMBER_99, status.getCode());
+        assertEquals("An error occurred", status.getMessage());
+    }
+
+    /**
+     * Method under test: {@link ActInquiryResponseMapper#fromException(RaddGenericException)}
+     */
+    @Test
+    void testFromException5() {
+        ActInquiryResponse actualFromExceptionResult = ActInquiryResponseMapper
+                .fromException(new RaddGenericException(ExceptionTypeEnum.DOCUMENT_UNAVAILABLE));
+        assertFalse(actualFromExceptionResult.getResult());
+        ActInquiryResponseStatus status = actualFromExceptionResult.getStatus();
+        assertEquals(ActInquiryResponseStatus.CodeEnum.NUMBER_4, status.getCode());
+        assertEquals("Documenti non disponibili", status.getMessage());
+    }
+
+    /**
+     * Method under test: {@link ActInquiryResponseMapper#fromException(RaddGenericException)}
+     */
+    @Test
+    void testFromException6() {
+        ActInquiryResponse actualFromExceptionResult = ActInquiryResponseMapper
+                .fromException(new RaddGenericException(ExceptionTypeEnum.QR_CODE_VALIDATION));
+        assertFalse(actualFromExceptionResult.getResult());
+        ActInquiryResponseStatus status = actualFromExceptionResult.getStatus();
+        assertEquals(ActInquiryResponseStatus.CodeEnum.NUMBER_1, status.getCode());
+        assertEquals("QrCode non valido", status.getMessage());
+    }
+
+    /**
+     * Method under test: {@link ActInquiryResponseMapper#fromException(RaddGenericException)}
+     */
+    @Test
+    void testFromException7() {
+        ActInquiryResponse actualFromExceptionResult = ActInquiryResponseMapper
+                .fromException(new RaddGenericException(ExceptionTypeEnum.DOCUMENT_NOT_FOUND));
+        assertFalse(actualFromExceptionResult.getResult());
+        ActInquiryResponseStatus status = actualFromExceptionResult.getStatus();
+        assertEquals(ActInquiryResponseStatus.CodeEnum.NUMBER_2, status.getCode());
+        assertEquals("Documenti non più disponibili", status.getMessage());
     }
 }
